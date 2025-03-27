2ad5f31693761dc214f8bcc4a9af77bc4ecfa6ff
mashrafi <ahmedfirojmash365@gmail.com>
2025-03-28 00:03:55 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const countLabel = document.getElementById('countLabel')
+const increaseBtn = document.getElementById('increaseBtn')
+const decreaseBtn = document.getElementById('decreaseBtn')
+const resetBtn = document.getElementById('resetBtn')
+
+let count = 0;
+let interval;
+
+increaseBtn.onclick = ()=>{
+    interval = setInterval(()=>{
+        count ++;
+        countLabel.textContent = count
+    },1000)
+    
+}
+
+decreaseBtn.onclick = ()=>{
+    clearInterval(interval)
+    interval = setInterval(()=>{
+        count --;
+        countLabel.textContent = count
+    },1000)
+}
+
+resetBtn.onclick = ()=>{
+    clearInterval(interval)
+    count = 0 ;
+    countLabel.textContent = count
+}
